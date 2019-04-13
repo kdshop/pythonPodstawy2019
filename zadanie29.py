245021f80f37648ac73ceea8579e457a62fcae3e
Konrad Albrecht <kontakt@konradalbrecht.pl>
2019-04-13 22:37:57 +0200

    laborki nr 5

--- zadanie29.py
+++ zadanie29.py
@@ -8,6 +8,50 @@ class Automat:
     def __init__(self):
         self.produkty = []
 
+    def dodajnapoje(self, napoje):
+        if type(napoje) == type([]):
+            for i in napoje:
+                if isinstance(i, Napoj):
+                    self.produkty.append(i)
+        else:
+            print("Prosze podaj arrayke z instancjami napojow")
+
     def wyswietlprodukty(self):
-        for produkt in self.produkty:
-            print(produkt.nazwa, produkt.cena)
+        for i in range(len(self.produkty)):
+            print(i, '. ', self.produkty[i].nazwa, self.produkty[i].cena)
+        print('')
+
+    def kupnapoj(self, moneta, wybor):
+        if self.produkty[wybor].cena > moneta:
+            print("Za malo funduszy")
+        if self.produkty[wybor].cena <= moneta:
+            print("Reszta: ", moneta - self.produkty[wybor].cena)
+
+
+a = 1
+instancje = []
+automat = Automat()
+
+automat.dodajnapoje([
+    Napoj('Sprite', 1),
+    Napoj('Fanta', 2),
+    Napoj('Cola', 3),
+    Napoj('Neste', 2)
+])
+
+while a != 'x':
+    print("1 - Wyswietl napoje.")
+    print("2 - Kup napoj")
+    print("x - Zakoncz dzialanie programu")
+
+    a = input("Wbierz opcje:")
+
+    if a == '1':
+        automat.wyswietlprodukty()
+    if a == '2':
+        automat.wyswietlprodukty()
+        automat.kupnapoj(int(input("Wrzuc monete")), int(input("Wybierz napoj z listy")))
+    elif a == 'x':
+        break
+    else:
+        print('Niewłaściwa opcja')
---
+++ zadanie30.py
@@ -0,0 +1,45 @@
+class Punkt:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def przesun(self, kierunek, wartosc):
+        if kierunek == 'x':
+            self.x += wartosc
+        if kierunek == 'y':
+            self.y += wartosc
+        if kierunek == 'z':
+            self.z += wartosc
+
+    def wyswietlpunkt(self):
+        print('x: ', self.x, ' y: ', self.y, ' z: ', self.z)
+
+    def obrocpunkt(self):
+        print("Tutaj powinna sie znajdowac formula matematyczna na obrot punktu w przestrzeni jednak niestety jej nei znam :(")
+
+
+a = 1
+punkt = Punkt(0, 0, 0)
+
+while a != 'x':
+    print("1 - Zdefiniuj punkt")
+    print("2 - Przesun punkt")
+    print("3 - Obroc punkt")
+    print("4 - Wyswietl wspolrzedne")
+    print("x - Zakoncz dzialanie programu")
+
+    a = input("Wbierz opcje:")
+
+    if a == '1':
+        punkt = Punkt(int(input("Podaj x")), int(input("Podaj y")), int(input("Podaj z")))
+    if a == '2':
+        punkt.przesun(input("Wprowadz os przesuniecia x, y lub z"), int(input("Wprowadz wartosc przesuniecia")))
+    if a == '3':
+        punkt.obrocpunkt()
+    if a == '4':
+        punkt.wyswietlpunkt()
+    elif a == 'x':
+        break
+    else:
+        print('Niewłaściwa opcja')
